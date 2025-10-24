eaac64757385cac3ba138c3c0e388af8234af90f
Лен <elleniin.v@yandex.ru>
2025-10-24 13:12:31 +0300

    Updated portfolio

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() { // переключает (открывает или закрывает) гамбургер-меню 
+    const menu = document.querySelector(".menu-links"); // в переменную menu записывается элемент класса menu-links
+    const icon = document.querySelector(".hamburger-icon"); // в переменную icon записывается элемент класса hamburger-icon
+    menu.classList.toggle("open"); // добавляет или удаляет класс open в элементе menu-links
+    icon.classList.toggle("open"); // добавляет или удаляет класс open в элементе hamburger-icon
 }
 
